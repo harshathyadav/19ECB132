305f0c3c571a8f3c2d89b10ea65b804fb102f2fe
harshath <101540046+harshathyadav@users.noreply.github.com>
2022-05-31 08:11:11 +0000

    ,,,,

--- doublelinkedlist.c
+++ doublelinkedlist.c
@@ -241,3 +241,62 @@ void search()
         }  
     }          
 }  
+/* output 
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+1
+
+Enter Item value1
+
+Node inserted
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+5 
+
+Enter item which you want to search?
+3
+Item not found
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+  7 */
